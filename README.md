9e2015f65ff31725d2f51ca6cecba4e6f60aa1a9
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2021-12-26 21:34:52 +0100

    counter done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # projects
+some very simple projects to remeber the basics and advance on the way!
--- counter/main.js
+++ counter/main.js
@@ -1,6 +1,13 @@
+// a simple counter project to get more comfortable with the basics of javascript! 
+
 let count = 0;
 let counter = document.getElementById("count-ele");
+/* init declaration to the count as 0
+   using the DOM to get the id of the counter.
+   setting the counter to the count variable as it increment.
+   doing the same for the variable as it decrements.
 
+*/
 function increment() {
     count++;
   counter.innerText = count;
