7b253572577c7cf19cbf2a879bf81b445ce05ff7
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2021-12-26 21:21:49 +0100

    still refresh saving

---
+++ counter/main.js
@@ -0,0 +1,14 @@
+let count = 0;
+let counter = document.getElementById("count-ele");
+
+function increment() {
+    count++;
+  counter.innerText = count;
+
+}
+
+function decrement(){
+    count--
+    counter.innerText = count
+    
+}
