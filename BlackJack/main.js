1c9889c8d1e29d3b6c68edce3cc337ec3480df99
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2021-12-28 19:54:37 +0100

    random cards

--- BlackJack/main.js
+++ BlackJack/main.js
@@ -1,12 +1,25 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cardsArray = [firstCard,secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive =true ;
+let isAlive = true ;
 let message = ""
 let messageEl = document.getElementById('message-el');
 let sumEl = document.getElementById('sum-el');
-function startGame() {
+let cardsEl = document.querySelector('#cards-el');
+function startGame  () {
+    renderGame();
+}
+function getRandomCard () {
+    return Math.floor(Math.random()*11)
+}
+function renderGame() {
+    cardsEl.textContent =  " Cards : " 
+    for (var i=0;i<cardsArray.length;i++) {
+        cardsEl.textContent +=  cardsArray[i] + " " 
+        sumEl.textContent = " sum :" + sum + " ";
+
     if(sum<=20){
         message = "Do you want to draw a new card? ";
         // isAlive = true;
@@ -20,5 +33,13 @@ function startGame() {
         isAlive = false;
     }
     messageEl.textContent = message
-    sumEl.textContent+=sum + " ";
+
+}}
+function newCard() {
+    // created a new variable named newCard hardCodded its value to 7 we should now get a blackJack since firstC = 10 and secondC = 4 , however those values should be random
+    // we add the new var to the sum then we call the function that will judge if we won or not.
+    let newCard = 7;
+    sum+= newCard;
+    cardsArray.push(newCard)
+    renderGame();
 }
